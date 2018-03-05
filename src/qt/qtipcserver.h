1f6119becf332e1a5c4f0b4b59fe67330d93217c
Caponecoin <caponecoin@gmail.com>
2018-03-05 15:42:05 +0100

    MODERDONIACASH-MODERCASH-MDDC
    MODERCASH (MDDC) es una cryptomoneda sin ánimo de lucro. Si alguien de
    antiguonia te las intenta vender te estará engañando(como siempre).
    Basado en NIST5
    rpcport=11935
    port=11936
    Total a emitir= 2100000000 (2,1 Billions LoL )
    Total preminado=100000000 (para repartir entre los moderdonios y hacer
    sobornos....)
    De momento y porque soy mas pobre que las ratas, no tengo unas DNsSeed
    decentes, con lo que es necesario conectar a mano.
    Puedes alojar la cartera en un pc cualquiera que esté conectado 24/7 o
    en un servidor a cambio de 1000MDDC, me das la IP y recompilo el código
    con HardNodes :)
    Prueba de que el soft no tiene bicho
    
    https://www.virustotal.com/es/file/0e52c16aa75b006866f1d3f2d8179c75c938dc0731963fad1d3f79c21707f3ea/analysis/1520258642/

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define MODERCASH-Qt message queue name
+#define MODERCASHURI_QUEUE_NAME "MODERCASHURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
